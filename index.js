8a513b334e40e0f14dede15e6d1def366b6b8d5a
Jeremy Che <jeremy.che48@gmail.com>
2018-01-25 23:38:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holdiayCountdown(days, holdiay) {
+function holidayCountdown(days, holdiay) {
   return `It\'s ${days} days until ${holdiay}!`
 }
